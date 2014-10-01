19024e1effe83f936f5a6981d4465320b213361e
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2014-10-01 12:29:56 +0200

    Fixed Sublime Text plugin installation

--- preconfigs/cmder/cygwin/opt/install_sublime_text_plugins.sh
+++ preconfigs/cmder/cygwin/opt/install_sublime_text_plugins.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 mkdir -p \
-     '/usr/local/bin/sublime-text/Installed Packages/'
+     '/usr/local/bin/sublime-text/Data/Installed Packages/'
 wget 'https://sublime.wbond.net/Package Control.sublime-package' \
-  -O '/usr/local/bin/sublime-text/Installed Packages/Package Control.sublime-package'
+  -O '/usr/local/bin/sublime-text/Data/Installed Packages/Package Control.sublime-package'
 
-mkdir -p '/usr/local/bin/sublime-text/Packages/User/'
+mkdir -p \
+   '/usr/local/bin/sublime-text/Data/Packages/User/'
 cp '/opt/utils/Package Control.sublime-settings' \
-   '/usr/local/bin/sublime-text/Packages/User/Package Control.sublime-settings'
+   '/usr/local/bin/sublime-text/Data/Packages/User/Package Control.sublime-settings'
+cp '/opt/utils/Preferences.sublime-settings' \
+   '/usr/local/bin/sublime-text/Data/Packages/User/Preferences.sublime-settings'
 
 wget 'https://raw.githubusercontent.com/MartinThoma/glpk/master/GLPK.sublime-build' \
   -O '/usr/local/bin/sublime-text/Data/Packages/User/GLPK.sublime-build'
